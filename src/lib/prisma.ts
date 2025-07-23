8988c1adee129ba84636f503cf9a8c228c0ddccc
Mathus Andrade <msmatheus14@gmail.com>
2025-07-23 17:24:31 -0400

    ALTERAÇÕES SUTIS

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,6 @@
 import { PrismaClient } from "@prisma/client"
 
-// Garante que apenas uma instância do PrismaClient seja criada em desenvolvimento
-// para evitar problemas de "hot-reloading" com múltiplas conexões.
+
 declare global {
   var prismaInstance: PrismaClient | undefined
 }
